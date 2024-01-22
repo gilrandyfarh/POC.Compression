83f7ea31d90afb417a7cd9792ff3bd8dfc2066fc
gilrandyfarhanadam <rendygfa@gmail.com>
2024-01-22 22:09:45 +0100

    Final

---
+++ POC.Sample/Controllers/ConditionalResponseCompressionMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System.IO.Compression;
+
+namespace POC.Sample.Middle;
+
+public class ConditionalResponseCompressionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ConditionalResponseCompressionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var originalBodyStream = context.Response.Body;
+
+        using var memoryStream = new MemoryStream();
+        context.Response.Body = memoryStream;
+
+        await _next(context);
+
+        if (ShouldCompress(context.Response.ContentLength))
+        {
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            using (var compressedStream = new MemoryStream())
+            {
+                using (var compressionStream = new BrotliStream(compressedStream, CompressionLevel.Fastest))
+                {
+                    await memoryStream.CopyToAsync(compressionStream);
+                }
+
+                context.Response.Headers.Add("Content-Encoding", "br");
+                context.Response.Headers.Remove("Content-Length");
+                context.Response.ContentLength = compressedStream.Length;
+
+                compressedStream.Seek(0, SeekOrigin.Begin);
+                await compressedStream.CopyToAsync(originalBodyStream);
+            }
+        }
+        else
+        {
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            await memoryStream.CopyToAsync(originalBodyStream);
+        }
+    }
+
+    private static bool ShouldCompress(long? contentLength)
+    {
+        // Adjust the threshold based on your requirements
+        return contentLength.HasValue && contentLength.Value > 1000;
+    }
+}
---
+++ POC.Sample/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.Providers.Add<BrotliCompressionProvider>();
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/json", "text/json" });
+    opts.EnableForHttps = true;
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(opts =>
+{
+    opts.Level = System.IO.Compression.CompressionLevel.Fastest;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseResponseCompression();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
